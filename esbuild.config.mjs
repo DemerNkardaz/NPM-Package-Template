fd3c6ec87206c55c1b960f89df7bcce532633c23
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2026-06-05 22:30:26 +0300

    Template ESBuild / Workflow update

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -12,7 +12,6 @@ await rm('dist', {
 });
 
 const common = {
-	entryPoints: ['src/index.ts'],
 	bundle: true,
 	treeShaking: true,
 	sourcemap: true,
@@ -22,16 +21,63 @@ const common = {
 	tsconfig: 'tsconfig.build.json',
 };
 
-const resultMJS = await build({ ...common, format: 'esm', outfile: 'dist/index.mjs' });
-const resultCJS = await build({ ...common, format: 'cjs', outfile: 'dist/index.cjs' });
+/*
+ * You additional imports for index here, example:
+	const yourImportMJS = await build({
+		...common,
+		entryPoints: ['src/your-import.ts'],
+		format: 'esm',
+		outfile: 'dist/your-import.mjs',
+	});
+	const yourImportCJS = await build({
+		...common,
+		entryPoints: ['src/your-import.ts'],
+		format: 'cjs',
+		outfile: 'dist/your-import.cjs',
+	});
 
-await writeFile('dist/meta-cjs.json', JSON.stringify(resultCJS.metafile));
+	await writeFile('dist/your-import-meta-esm.json', JSON.stringify(yourImportMJS.metafile));
+	await writeFile('dist/your-import-meta-cjs.json', JSON.stringify(yourImportCJS.metafile));
+*/
+
+const externalImportsPlugin = (format) => ({
+	name: 'external-imports',
+	setup(build) {
+		build.onResolve({ filter: /.*/ }, (args) => {
+			// if (args.importer && args.path.includes('/your-import')) {
+			// 	return {
+			// 		path: format === 'esm' ? './your-import.mjs' : './your-import.cjs',
+			// 		external: true,
+			// 	};
+			// }
+		});
+	},
+});
+
+const resultMJS = await build({
+	...common,
+	entryPoints: ['src/index.ts'],
+	format: 'esm',
+	outfile: 'dist/index.mjs',
+	plugins: [externalImportsPlugin('esm')],
+});
+const resultCJS = await build({
+	...common,
+	entryPoints: ['src/index.ts'],
+	format: 'cjs',
+	outfile: 'dist/index.cjs',
+	plugins: [externalImportsPlugin('cjs')],
+});
+
 await writeFile('dist/meta-esm.json', JSON.stringify(resultMJS.metafile));
+await writeFile('dist/meta-cjs.json', JSON.stringify(resultCJS.metafile));
 
-const distFiles = readdirSync('dist').filter((f) => !f.endsWith('.map'));
+const distFiles = readdirSync('dist').filter((f) => !f.endsWith('.map') && !f.endsWith('.json'));
 const totalSize = distFiles.reduce((sum, f) => sum + statSync(`dist/${f}`).size, 0);
 
 if (totalSize > limit) {
 	console.log('\x1b[33m%s\x1b[0m', `Bundle too large: ${totalSize} > ${limit}`);
 	process.exit(1);
+} else {
+	console.log('\x1b[32m%s\x1b[0m', `Bundle size: ${totalSize} < ${limit}`);
 }
