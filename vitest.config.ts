d032c1c00f23afb9d22b6ddb082ec6f313adb0e0
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2026-06-05 12:31:08 +0300

    Return to node 24 26

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vitest/config';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+	resolve: {
+		tsconfigPaths: true,
+	},
 	test: {
 		globals: true,
 		environment: 'node',
